7511f1b1cbd632eb1f805a6decdbdd9d4cf182e1
anandsagarchaurasiya <anandsagarch35@gmail.com>
2026-09-07 01:48:51 +0530

    added

---
+++ if_else/oddeven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+ int n ;
+ printf("Eneter you number : ");
+ scanf("a%d",&n);
+ if(n%2==0)
+ {
+ printf("Even number");
+ }
+ else{
+  printf("odd number");
+ }
+ 
+ 
+}
